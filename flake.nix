94abaed787f00ec2a590fa230145e0aa3f3116da
Marven11 <110723864+Marven11@users.noreply.github.com>
2026-06-04 09:26:12 +0800

    Update dependency

--- flake.nix
+++ flake.nix
@@ -1,44 +1,37 @@
 {
-  description = "Python venv development template";
+  description = "Image webshell generator";
 
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    nixpkgsOld.url = "https://github.com/NixOS/nixpkgs/archive/b4e193a23a1c5d8794794e65cabf1f1135d07fd9.tar.gz";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs =
     { self
     , nixpkgs
-    , nixpkgsOld
     , utils
     , ...
     }:
     utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
-      pkgsOld = import nixpkgsOld { inherit system; };
       pythonPackages = pkgs.python3Packages;
-      python37Packages = pkgsOld.python37Packages;
     in
     {
       devShells.default = pkgs.mkShell {
-        name = "python-venv";
+        name = "image-webshell";
         venvDir = "./.venv";
         buildInputs = with pythonPackages; [
           python
           venvShellHook
-          notebook
-          ipython
           pillow
           fire
           numpy
         ];
         postVenvCreation = ''
           unset SOURCE_DATE_EPOCH
-          pip install -r requirements.txt
         '';
         postShellHook = ''
-          # allow pip to install wheels
           unset SOURCE_DATE_EPOCH
         '';
       };
